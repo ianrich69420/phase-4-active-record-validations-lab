f6cf95d234b8f1c38af4b2bfb4bc01ffa8f2fa34
ian <ian.orieko@student.moringaschool.com>
2023-04-02 02:55:28 +0300

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :title_is_sufficiently_clickbait_y
+
+    def title_is_sufficiently_clickbait_y
+        unless title && title.match?(/(Won't Believe)|(Secret)|(Guess)|(Top\s\d)/)
+            errors.add(:title, "title is insufficiently clickbait-y")
+        end
+    end
 end
